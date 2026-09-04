91330d5ff0fa2da789a740c449a2d0f9d2d743ee
Estefanio Dunice Lordes <estefanio58381116@edu.df.senac.br>
2026-09-04 10:11:31 -0300

    feat: adiciona render.yaml, compatibilidade com Render e ajusta CORS/PORT

--- ConectaTi-senac/backend/conectati-api/conectati-api/src/main/java/br/com/senac/conectati/config/CorsConfig.java
+++ ConectaTi-senac/backend/conectati-api/conectati-api/src/main/java/br/com/senac/conectati/config/CorsConfig.java
@@ -20,8 +20,12 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
 
                 registry.addMapping("/**")
-                        // Durante o desenvolvimento
-                        .allowedOrigins("http://localhost:3000", "http://127.0.0.1:3000", "http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:5173", "http://127.0.0.1:5173")
+                        .allowedOriginPatterns(
+                                "http://localhost:*",
+                                "http://127.0.0.1:*",
+                                "https://*.onrender.com",
+                                "*"
+                        )
 
                         // Métodos HTTP permitidos
                         .allowedMethods(
